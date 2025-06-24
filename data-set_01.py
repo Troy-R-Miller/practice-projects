a2eb141d958088ab38e4611e73301e6ba4389ffc
Troy-R-Miller <troy.rich.miller@outlook.com>
2025-06-23 20:00:36 -0400

    data-set_01.py

--- data-set_01.py
+++ data-set_01.py
@@ -1,4 +1,4 @@
-#Extract integers from data-set.txt
+#Extract raw data from data-set.txt -> convert to integers
 import re
 datafile = input("Enter a file name: ")
 if len(datafile) < 1:
@@ -13,13 +13,13 @@ except:
 #Create list to store values
 values = list()
 
-#Extracting integers from data-set.txt and adding to values list
+#Extracting values from data-set.txt and adding to values list
 for line in dfile:
-    nums = re.findall('([0-9]+)', line) #extracting integers from data-set.txt
+    nums = re.findall('([0-9]+)', line) #extracting values for integer conversion from data-set.txt
     values = values + nums
 print(values)
 
-#Calculating sum of integers
+#Conversion and calculations of integers
 total = 0
 for numbers in values:
     total=total+int(numbers)
